c86e1017ea95da4dc2bc352360b6339c385900c8
Vance_100 <evanslance100@gmail.com>
2024-11-05 23:35:39 +0300

    https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful/description/

--- min_num_of_changes.py
+++ min_num_of_changes.py
@@ -7,8 +7,8 @@ class Solution:
         for i in range(0, len(s_list) - 1, 2):
             # Check if the current character and the next are different
             if s_list[i] != s_list[i + 1]:
-                # Change the next character to match the current one
+                # Change the next character to match the current 1
                 s_list[i + 1] = s_list[i]
                 change_made += 1
-        
+                
         return change_made
