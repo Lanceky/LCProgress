82a3dc05a55e0fbfbce5ebe6c1a1987f8fdc5e5f
Vance_100 <evanslance100@gmail.com>
2024-12-01 04:58:28 +0300

    Update gcd.py

--- LeetCode 75/Greatest Common Divisor of Strings/gcd.py
+++ LeetCode 75/Greatest Common Divisor of Strings/gcd.py
@@ -5,12 +5,15 @@ class Solution:
             return ""
         
         # Define a custom GCD function
+        #uses the Euclidean algorithm to find GCD
+        #Untill becomes zero, swap a and b so that a becomes b and b becomes the remainder of a % b
         def gcd(a, b):
             while b != 0:
                 a, b = b, a % b
             return a
         
         # Compute the GCD of the lengths of the two strings
+        # Why? If two strings have a common substring, the length of the substring must divide the lengths of both strings evenly
         gcd_length = gcd(len(str1), len(str2))
         
         # The GCD string is the prefix of str1 with the GCD length
